ee22ab2f8fb34d6720ff2fa120389bf7498db0d7
ImNotAVirus <s.soulie@hotmail.fr>
2019-02-27 22:55:49 +0100

    :sparkles: Create modules for services

---
+++ apps/elven_gard/lib/elven_gard/services/generic_service.ex
@@ -0,0 +1,6 @@
+defmodule ElvenGard.Services.GenericService do
+  @moduledoc """
+  TODO: Documentation for ElvenGard.Services.GenericService
+  """
+
+end
---
+++ apps/elven_gard/lib/elven_gard/services/stateless_service.ex
@@ -0,0 +1,6 @@
+defmodule ElvenGard.Services.StatelessService do
+  @moduledoc """
+  TODO: Documentation for ElvenGard.Services.StatelessService
+  """
+
+end
